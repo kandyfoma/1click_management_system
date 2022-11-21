625cd1a1f0db256c34aba9606ee7d5cc21bb6237
fomakyala <kandy@siege.ai>
2022-11-21 13:13:12 +0200

    deploy

--- inventory/settings.py
+++ inventory/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+import django_on_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 AUTH_USER_MODEL = 'users.User'
 # ####################### JAZZMIN SETTINGS ##################################
 
